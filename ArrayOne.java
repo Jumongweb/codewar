556a11caa8af7731895c2b548e5495c4b336cb9f
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-12-07 17:21:28 +0100

    array fix

--- ArrayOne.java
+++ ArrayOne.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 public class ArrayOne{
-public static void main(String[] args){
+	public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
 
-int[] numbers = {1,2,3,4,5};
-numbers[0] = numbers[0] * numbers[0];
-numbers[1] = numbers[1] * numbers[1];
-numbers[2] = numbers[2] * numbers[2];
-numbers[3] = numbers[3] * numbers[3];
-numbers[4] = numbers[4] * numbers[4];
+	int[] numbers = {1,2,3,4,5};
+	numbers[0] = numbers[0] * numbers[0];
+	numbers[1] = numbers[1] * numbers[1];
+	numbers[2] = numbers[2] * numbers[2];
+	numbers[3] = numbers[3] * numbers[3];
+	numbers[4] = numbers[4] * numbers[4];
 
 for (int nums = 0; nums < numbers.length; nums++){
 	System.out.println(numbers[nums]);
