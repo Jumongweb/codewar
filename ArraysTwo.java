243fac4c4d2293cb786be2482ca6b8d47c30ced9
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-12-07 17:19:07 +0100

    Array two

---
+++ ArraysTwo.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class ArrayTwo{
+public static void main(String[] args){
+
+String[] str = {"ale", "apple", "monkey", "plea"};
+
+
+for (int nums = 0; nums < str.length; nums++){
+	System.out.println(str[nums]);
+}
+
+
+}
+}
