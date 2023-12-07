1b9f1cb047ec92628471bcc0a8a6ab2f3d6c25dc
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-12-07 17:11:29 +0100

    first

---
+++ ArrayOne.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class ArrayOne{
+public static void main(String[] args){
+
+int[] numbers = {1,2,3,4,5};
+numbers[0] = numbers[0] * numbers[0];
+numbers[1] = numbers[1] * numbers[1];
+numbers[2] = numbers[2] * numbers[2];
+numbers[3] = numbers[3] * numbers[3];
+numbers[4] = numbers[4] * numbers[4];
+
+for (int nums = 0; nums < numbers.length; nums++){
+	System.out.println(numbers[nums]);
+
+}
+
+
+}
+}
